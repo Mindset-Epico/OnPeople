3463ce398a48108353de7b8748ba3782796cbc6c
Pedro Braga <pedro.braga@prevenirassistencial.com.br>
2023-04-17 21:54:20 -0300

    Backend V1

---
+++ Front/ProEventos-App/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { EventosComponent } from './components/eventos/eventos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { MetasComponent } from './components/metas/metas.component';
+
+const routes: Routes = [
+  { path: 'eventos', component: EventosComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'palestrantes', component: PalestrantesComponent },
+  { path: 'contatos', component: ContatosComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'metas', component: MetasComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
